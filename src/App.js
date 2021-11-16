a5788cf6165ab21b798f0b3049f8a22aa40ddb0a
Sabitzhan Altynay <s2100149@edu.bc.fi>
2021-11-16 12:27:52 +0200

    disabled added to the button

--- src/App.js
+++ src/App.js
@@ -14,18 +14,29 @@ class App extends Component {
     current: 0,
     gameOver: false,
     //each of circle has their own id and color
+    pace: 1500,
+    rounds: 0,
   };
 
   timer = undefined;
-  pace = 1500;
 
-  clickHandler = () => {
+  clickHandler = (id) => {
+    console.log("you click", id);
+    if (this.state.current !== id) {
+      this.startHandler();
+      return; //return will stop it
+    }
     this.setState({
       score: this.state.score + 10,
+      rounds: 0,
     });
   };
 
   nextCircle = () => {
+    if (this.state.rounds >= 5) {
+      this.stopHandler();
+      return; //if u found it, then stop here
+    }
     let nextActive;
 
     do {
@@ -34,30 +45,47 @@ class App extends Component {
 
     this.setState({
       current: nextActive,
+      pace: this.state.pace * 0.95,
+      rounds: this.state.rounds + 1,
     });
 
-    this.pace *= 0.95;
-
-    this.timer = setTimeout(this.nextCircle, this.pace);
+    this.timer = setTimeout(this.nextCircle, this.state.pace);
 
     console.log("active circles is", this.state.current);
+    console.log("round number", this.state.current);
   };
 
   startHandler = () => {
     this.nextCircle();
+    this.setState({
+      gameStart: true,
+    });
   };
 
   stopHandler = () => {
     clearTimeout(this.timer);
     this.setState({
       gameOver: true,
+      current: 0,
+      gameStart: false,
+    });
+  };
+
+  closeHandler = () => {
+    this.setState({
+      gameOver: false,
+      score: 0,
+      pace: 1500,
+      rounds: 0,
     });
   };
 
   render() {
     return (
       <div>
-        {this.state.gameOver && <GameOver score={this.state.score} />}
+        {this.state.gameOver && (
+          <GameOver score={this.state.score} close={this.closeHandler} />
+        )}
         <div className="text">
           <h1>Speed Game</h1>
           <p>Your score is:{this.state.score}</p>
@@ -70,14 +98,16 @@ class App extends Component {
                 key={c.id}
                 color={c.color}
                 id={c.id}
-                whatever={this.clickHandler}
+                whatever={() => this.clickHandler(c.id)} //finding the data on the event
                 active={this.state.current === c.id}
               />
             ); //when using mapping, react wants to know the specific element, so elements has to have id, key is used for that
           })}
         </div>
         <div className="button_container">
-          <button onClick={this.startHandler}>Start</button>
+          <button disabled={this.state.gameStart} onClick={this.startHandler}>
+            Start
+          </button>
           <button onClick={this.stopHandler}>Stop</button>
         </div>
       </div>
@@ -86,3 +116,5 @@ class App extends Component {
 }
 
 export default App;
+
+//
--- src/GameOver.js
+++ src/GameOver.js
@@ -1,16 +1,12 @@
 import React from "react";
 
-const closeHandler = () => {
-  window.location.reload();
-};
-
 const GameOver = (props) => {
   return (
-    <div className="overlay" onChick={closeHandler}>
+    <div className="overlay">
       <div className="popup">
         <h2>Game over</h2>
         <p>Score was :{props.score}</p>
-        <button onClick={closeHandler}>X</button>
+        <button onClick={props.close}>X</button>
       </div>
     </div>
   );
